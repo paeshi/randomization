f97e5072439a435cc357e702bbbc3742424f204e
stevepae <stevepaemusic@gmail.com>
2021-03-05 15:09:19 -0500

    added new Random features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// quotable api
 const $content = $('#content');
-$('button').on('click', function(e) {
+$('button#moreContent').on('click', function(e) {
     e.preventDefault();
     let endpoint = 'https://api.quotable.io/random';
     $.ajax({
@@ -11,9 +12,54 @@ $('button').on('click', function(e) {
         });
     })
 
+    
+// bored api https://www.boredapi.com/documentation#endpoints-random
+const $activity = $('#activity');
+    $('button#bored').on('click', function(e) {
+        e.preventDefault();
+        let endpoint2 = 'http://www.boredapi.com/api/activity/';
+        $.ajax({
+        url: endpoint2,
+        success: function(result) {
+            $activity.text(`${result.activity}`);
+            
+            }
+        });
+    })
 
 
+//    random poem api
+const $poemTitle = $('#poemTitle');
+const $poemContent = $('#poemContent');
+const $poet = $('#poet');
+    $('button#newPoem').on('click', function(e) {
+        e.preventDefault();
+        let endpoint3 = 'https://www.poemist.com/api/v1/randompoems';
+        $.ajax({
+            url: endpoint3,
+            success: function(result) {
+                $poemTitle.text(`${result[0].title}`);
+                $poemContent.text(`${result[0].content}`) 
+                $poet.text(`-${result[0].poet.name}`);
+                
+            }
+        });
+    })
 
+const $dog = $('#dog');
+    $('button#newDog').on('click', function(e) {
+        e.preventDefault();
+        $.ajax({
+        url: "https://dog.ceo/api/breeds/image/random",
+        success: function(result) {
+        $dog.attr("src", `${result.message}`);
+                   
+                    
+            }
+        });
+    });
+
+           
 
 
 
