81bce8a06acc38ea5aa0e504be12e1024a823e9e
stevepae <stevepaemusic@gmail.com>
2021-03-05 09:17:44 -0500

    updated to cleaner code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-let quote;
-
 const $content = $('#content');
-    let endpoint = "https://api.quotable.io/random";
+$('button').on('click', function(e) {
+    e.preventDefault();
+    let endpoint = 'https://api.quotable.io/random';
     $.ajax({
         url: endpoint,
-        contentType: "application/json",
         success: function(result) {
-            $content.text(`" ${result.content}" —${result.author}`);
+            $content.text(`"${result.content}" —${result.author}`);
+            
         }
-    });
-  
-    function refreshPage(){
-        window.location.reload();
-    } 
-
-
+        });
+    })
 
 
 
