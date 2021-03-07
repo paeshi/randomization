b74d170e44b92d4f922712b80b236b2cd96e928e
stevepae <stevepaemusic@gmail.com>
2021-03-07 16:41:58 -0500

    updated layout and added more apis

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 // quotable api
-const $content = $('#content');
+const $content = $('#quote-content');
 $('button#moreContent').on('click', function(e) {
     e.preventDefault();
     let endpoint = 'https://api.quotable.io/random';
     $.ajax({
         url: endpoint,
         success: function(result) {
-            $content.text(`"${result.content}" —${result.author}`);
+            $content.html('" '+ result.content +'" ' + 
+            '<br/><br/>' + '-' + result.author)
             
         }
         });
     })
 
     
-// bored api https://www.boredapi.com/documentation#endpoints-random
 const $activity = $('#activity');
     $('button#bored').on('click', function(e) {
         e.preventDefault();
+        $('#bored-container').animate({
+            width: '50%',
+            height: '100%' 
+        }, 1000, function() {
+
+        });
         let endpoint2 = 'http://www.boredapi.com/api/activity/';
         $.ajax({
         url: endpoint2,
@@ -26,7 +32,29 @@ const $activity = $('#activity');
             }
         });
     })
+    $('#heading3div').hide(); 
+    $('#bored-container').mouseover(function() {
+        $('#heading3div').fadeIn(500, 'linear');
+    })
 
+   
+$('#bluefeather').hide();
+$('#heading1').mouseover(function() {
+    $('#bluefeather').fadeIn(500, 'swing');
+});
+$('#p1message').mouseover(function() {
+    $('#bluefeather').fadeOut(1000, 'swing');
+})
+
+
+    
+$('#heading2div').hide(); 
+    $('#moreContent').mouseover(function () {
+        $('#heading2div').fadeIn(2000, 'linear'); 
+        console.log('works');
+    });
+
+    
 
 //    random poem api
 const $poemTitle = $('#poemTitle');
@@ -59,11 +87,37 @@ const $dog = $('#dog');
         });
     });
 
-           
+const $pexFrame = $('#pexFrame');
+const api_key = "563492ad6f917000010000014b61995ccd004085ba7dfe6c20ef2b8f"; 
 
 
+  $("#form").submit(function(e) {
+      e.preventDefault();
+        let search = $('#search').val();
+        let url = "https://api.pexels.com/v1/search?query="+ search +"&per_page=6";
+        
+        let image="";
+        $('#images').empty();
+        
+      $.ajax({
+          method: 'GET',
+          beforeSend: function(xhr) {
+              xhr.setRequestHeader ("Authorization", api_key);
+          },
+          url: url,
+          success: function (data) {
+            //   console.log(data)
+ 
+           data.photos.forEach(photo => {
+               image = `
+               <img src="${photo.src.original}" width="400" height="400"/>`
+               $("#images").append(image);
 
-
-
-
-   
+           })
+              
+          },
+          error: function (error) {
+              console.log(error);
+          }
+      });
+    });
